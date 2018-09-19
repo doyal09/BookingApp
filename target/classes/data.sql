d7d90f26bb929dc2725653b097f12b591750612c
dchoudhury <dchoudhury@UKVDI0160.aegonuk.com>
2018-09-19 10:31:35 +0100

    SQL query modification

--- target/classes/data.sql
+++ target/classes/data.sql
@@ -1,14 +1,14 @@
 insert into ROOM_DETAILS
-values(1,'Single','true');
+values(1,'Single');
 
 insert into ROOM_DETAILS
-values(2, 'Single','true');
+values(2, 'Single');
 
 insert into ROOM_DETAILS
-values(3, 'Double','true');
+values(3, 'Double');
 
 insert into ROOM_DETAILS
-values(4, 'Double','true');
+values(4, 'Double');
 
 insert into ROOM_DETAILS
-values(5, 'Double','true');
+values(5, 'Double');
